abe8746b2116c7c4a62a5bcd7cc99ec77914e358
dendritadev <dendritadev@gmail.com>
2023-09-28 20:38:08 +0200

    MyIkeaPlace Finished

--- ARFoundation/Assets/_Scripts/MyIkeaPlace/InputManager.cs
+++ ARFoundation/Assets/_Scripts/MyIkeaPlace/InputManager.cs
@@ -1,10 +1,10 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.Interaction.Toolkit.AR;
 
-public class InputManager : MonoBehaviour
+public class InputManager : ARBaseGestureInteractable
 {
 
     [SerializeField] private Camera ARCamera;
@@ -16,33 +16,53 @@ public class InputManager : MonoBehaviour
     private Touch touch;
     private Pose pose;
 
-    private void Update()
+    protected override bool CanStartManipulationForGesture(TapGesture gesture)
+    {
+        //si no colissiona con nada
+        if (gesture.TargetObject == null)
+            return true;
+        return false;
+    }
+
+    protected override void OnEndManipulation(TapGesture gesture)
+    {
+        if (gesture.WasCancelled)
+            return;
+
+        
+        if (gesture.targetObject != null || IsPointerOverUI(gesture))
+            return;
+
+        if (GestureTransformationUtility.Raycast(gesture.startPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+        {
+            GameObject placedObj = Instantiate(DataHandler.Instance.GetFurniture(), pose.position, pose.rotation);
+
+            var anchorObject = new GameObject("PlacementAnchor");
+            anchorObject.transform.position = pose.position;
+            anchorObject.transform.rotation = pose.rotation;
+            placedObj.transform.parent = anchorObject.transform;
+        }
+
+
+    }
+
+    private void FixedUpdate()
     {
         CrossHairCalculation();
-
-        touch = Input.GetTouch(0);
-
-        if (Input.touchCount < 0 || touch.phase != TouchPhase.Began)
-            return;
-
-        if (IsPointerOverUI(touch))
-            return;
-
-
-        Instantiate(DataHandler.Instance.GetFurniture(), pose.position, pose.rotation);
+        //Instantiate(DataHandler.Instance.GetFurniture(), pose.position, pose.rotation);
     }
 
     /// <summary>
-    /// Miramos si de los touch a la pantalla que hace el usuario, alguno choca con la UI, y por tanto, quiere decir que estaba pulsando UI y no que tenía la itnencionde spawnear muebles. Por tanto,
-    /// de ser así, devolvemos true y se hace return en el update
+    /// Miramos si de los touch a la pantalla que hace el usuario, alguno choca con la UI, y por tanto, quiere decir que estaba pulsando UI y no que tenÃ­a la itnencionde spawnear muebles. Por tanto,
+    /// de ser asÃ­, devolvemos true y se hace return en el update
     /// </summary>
     /// <param name="touch"> touch en la pantalla del cual cogemos su posicion para saber si choca con alguna UI</param>
     /// <returns></returns>
-    bool IsPointerOverUI(Touch touch)
+    bool IsPointerOverUI(TapGesture touch)
     {
         
         PointerEventData eventData = new PointerEventData(EventSystem.current);
-        eventData.position = new Vector2(touch.position.x, touch.position.y);
+        eventData.position = new Vector2(touch.startPosition.x, touch.startPosition.y);
         
         List<RaycastResult> results = new List<RaycastResult>();
         EventSystem.current.RaycastAll(eventData, results);
@@ -56,9 +76,7 @@ public class InputManager : MonoBehaviour
         //Centro de la pantalla
         Vector3 origin = ARCamera.ViewportToScreenPoint(new Vector3(0.5f, 0.5f, 0));
 
-        //Spawn Furniture
-        Ray ray = ARCamera.ScreenPointToRay(origin); //rayo al centro de la pantalla
-        if (raycastManager.Raycast(ray, hits))
+        if (GestureTransformationUtility.Raycast(origin, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
         {
             //Pose:  Representation of a Position and rotation of an object
             pose = hits[0].pose;
